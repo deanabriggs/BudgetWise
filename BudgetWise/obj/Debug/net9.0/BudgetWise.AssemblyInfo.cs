5f57a53d05be781ea1314abaf8fbd940097c3a6c
Cameron Woodward <woo23017@byui.edu>
2025-11-21 16:50:30 -0700

    Finished Totals, expenses, and budgets

--- BudgetWise/obj/Debug/net9.0/BudgetWise.AssemblyInfo.cs
+++ BudgetWise/obj/Debug/net9.0/BudgetWise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93de7ebefbe631b904bfd41e4954a94bf6738707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f788a00fb3093351a5ebdb6d1e4c43d95620a7ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
